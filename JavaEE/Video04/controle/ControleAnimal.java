e8f5da622f7a1b0034415859c1a4f3ce5bfaa319
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-04-12 18:39:45 -0300

    Add files via upload

--- JavaEE/Video04/controle/ControleAnimal.java
+++ JavaEE/Video04/controle/ControleAnimal.java
@@ -1,1 +1,14 @@
+package controle;
 
+import dominio.Animal;
+
+public class ControleAnimal {
+	Animal a = new Animal();
+	
+	public void cadastrarAnimal(String nome, int idade, float peso, String especie) {
+		a.setNome(nome);
+		a.setIdade(idade);
+		a.setPeso(peso);
+		a.setEspecie(especie);
+	}
+}
