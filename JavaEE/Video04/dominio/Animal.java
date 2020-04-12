dfef0919620f4557bb620d9e2980ab664396b0c2
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-04-12 18:39:05 -0300

    Create Animal.java

---
+++ JavaEE/Video04/dominio/Animal.java
@@ -0,0 +1,44 @@
+package dominio;
+
+public class Animal {
+	private String nome;
+	private int idade;
+	private float peso;
+	private String especie;
+	
+	public Animal() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+}
