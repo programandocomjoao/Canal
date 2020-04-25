12b0f9d8205a95d51dbaa51255f10f3364733411
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-04-25 16:59:23 -0300

    Add files via upload

--- JavaEE/Video06/controle/SalvarCliente.java
+++ JavaEE/Video06/controle/SalvarCliente.java
@@ -1,1 +1,25 @@
+package controle;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SalvarCliente")
+public class SalvarCliente extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String nome = request.getParameter("nome");
+		String telefone = request.getParameter("telefone");
+		String email = request.getParameter("email");
+		
+		response.sendRedirect("sucesso.jsp?nome=" + nome + "&telefone=" + telefone + "&email=" + email);
+	}
+}
