0129aee4c91b579c7ff5365bae93186445d39102
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2020-04-25 17:08:48 -0300

    Add files via upload

--- JavaEE/Video08/dominio/Sessao.java
+++ JavaEE/Video08/dominio/Sessao.java
@@ -1,1 +1,50 @@
+package dominio;
 
+public class Sessao {
+	private String filme, data, inicio, termino;
+	private int sala;
+	
+	public Sessao() {
+		
+	}
+
+	public String getFilme() {
+		return filme;
+	}
+
+	public void setFilme(String filme) {
+		this.filme = filme;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(String inicio) {
+		this.inicio = inicio;
+	}
+
+	public String getTermino() {
+		return termino;
+	}
+
+	public void setTermino(String termino) {
+		this.termino = termino;
+	}
+
+	public int getSala() {
+		return sala;
+	}
+
+	public void setSala(int sala) {
+		this.sala = sala;
+	}
+}
